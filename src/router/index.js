22ca48d1a3f729531a5a3b9f34d938f5028661d8
Oblasov Ilya <oblasovs@icloud.com>
2022-10-07 03:02:19 +0300

    route-fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [{
-  path: '/',
-  name: 'services',
-  component: () => import('../views/Services.vue')
-}];
+const routes = [
+  {
+    path: '/',
+    name: 'services',
+    component: () => import('../views/Services.vue')
+  },
+  {
+    path: '/404',
+    name: 'not-found',
+    component: () => import('../views/NotFound.vue')
+  }
+];
 
 const router = createRouter({
   history: createWebHistory('/apps'),
---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+/<template>
+    <div
+      style="
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        width: 100%;
+        min-height: 100vh;
+      "
+    >
+        <div style="margin: 0 10%">
+            <p class="display-5" style="word-wrap: break-word">
+            Вы попали на страницу, которой еще не существует в приложении =(
+        </p>
+        <p></p>
+        <p class="display-6">
+            Попробуйте <a href="javascript:history.go(-1)">вернуться назад</a>.
+        </p>
+        </div>
+    </div>
+</template>
