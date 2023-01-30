89077cdcc9ce6de1370f5aba7370a3c3780ed089
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2023-01-30 17:12:02 +0300

    Fix routing

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/apps',
-    name: 'services',
     component: () => import('../views/Services.vue')
   },
   {
     path: '/apps/browser',
-    name: 'browser',
     component: () => import('../views/Browser.vue')
   }
 ];
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 });
 
